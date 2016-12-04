2328af3969ad13ce270fe8eb1644950db1a946b4
rabinzon <ildarrazin@gmail.com>
2016-12-04 16:28:14 +0300

    feat(db): add logger

--- src/db.js
+++ src/db.js
@@ -1,15 +1,22 @@
 const redis = require('redis');
+const bunyan = require('bunyan');
 
 const client = redis.createClient();
+const log = bunyan.createLogger({
+	name: 'bot',
+	streams: [{
+		path: './foo.log',
+	}]
+});
 
 class DB {
 	constructor() {
-		client.on("error", (err) => console.log("Error: " + err));
+		client.on("error", (err) => log.info("Error: " + err));
 	}
 	save(id, val) {
 		client.hmset(id, val, err => {
 			if (err) {
-				console.log('Что то случилось при записи: ' + err);
+				log.info('Что то случилось при записи: ' + err);
 				client.quit();
 			}
 		});
